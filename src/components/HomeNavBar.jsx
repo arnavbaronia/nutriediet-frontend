e087ec362afe99d13675752f1b30a623ee81ca41
arnavbaronia <arnav.baronia@gmail.com>
2026-07-06 22:19:33 +0530

    login signup buttons fixed on landing page

--- src/components/HomeNavBar.jsx
+++ src/components/HomeNavBar.jsx
@@ -1,63 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import "../styles/HomeNavBar.css";
 import logo from "../assets/Nutriediet_Logo_Transparent.png";
-import { FiMenu } from "react-icons/fi";
 
 const HomeNavBar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
   const location = useLocation();
 
   const isActive = (path) => (location.pathname === path ? "active" : "");
 
-  const handleLinkClick = () => {
-    if (window.innerWidth <= 768) {
-      setMenuOpen(false);
-    }
-  };
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth > 768) {
-        setMenuOpen(false);
-      }
-    };
-  
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
   return (
     <nav className="navigation-bar">
       <div className="nav-logo">
-        <Link to="/" onClick={handleLinkClick}>
+        <Link to="/">
           <img src={logo} alt="Nutriediet Logo" className="nav-logo-img" />
         </Link>
       </div>
-      <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
-        <FiMenu />
-      </div>
-      <div className={`nav-buttons ${menuOpen ? "show-menu" : ""}`}>
-        {/* <Link to="/" className={`nav-link ${isActive("/")}`} onClick={handleLinkClick}>
-          Home
-        </Link> */}
-        {/* <Link to="/about" className={`nav-link ${isActive("/about")}`} onClick={handleLinkClick}>
-          About
+
+      <div className="auth-buttons">
+        <Link
+          to="/login"
+          className={`nav-button login-btn ${isActive("/login")}`}
+        >
+          Login
         </Link>
-        <Link to="/services" className={`nav-link ${isActive("/services")}`} onClick={handleLinkClick}>
-          Services
+
+        <Link
+          to="/signup"
+          className={`nav-button signup-btn ${isActive("/signup")}`}
+        >
+          Signup
         </Link>
-        <Link to="/testimonials" className={`nav-link ${isActive("/testimonials")}`} onClick={handleLinkClick}>
-          Testimonials
-        </Link> */}
-        <div className="auth-buttons">
-          <Link to="/login" className={`nav-button login-btn ${isActive("/login")}`} onClick={handleLinkClick}>
-            Login
-          </Link>
-          <Link to="/signup" className={`nav-button signup-btn ${isActive("/signup")}`} onClick={handleLinkClick}>
-            Signup
-          </Link>
-        </div>
       </div>
     </nav>
   );
